e3d1e69a6a6a2ebcc37b9139689da6113f93f74e
Zhadyrame <zhadyra.nadirkhan@narxoz.kz>
2025-02-08 14:31:14 +0500

    Добавил новые файлы и обновил код дронов

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        HeavyDrone heavyDrone = new HeavyDrone("HD-001");
+        LightDrone lightDrone = new LightDrone("LD-001");
+
+
+        heavyDrone.setStatus("Delivering");
+        lightDrone.setStatus("Idle");
+
+        System.out.println("Heavy Drone ID: " + heavyDrone.getId());
+        System.out.println("Status: " + heavyDrone.getStatus());
+        System.out.println("Cargo Capacity: " + heavyDrone.getCargoCapacity() + " kg");
+        System.out.println("Estimated Fly Time for 100 km: " + heavyDrone.flyTime(100) + " hours\n");
+
+        System.out.println("Light Drone ID: " + lightDrone.getId());
+        System.out.println("Status: " + lightDrone.getStatus());
+        System.out.println("Cargo Capacity: " + lightDrone.getCargoCapacity() + " kg");
+        System.out.println("Estimated Fly Time for 100 km: " + lightDrone.flyTime(100) + " hours");
     }
 }
